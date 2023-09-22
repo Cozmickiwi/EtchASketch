3d8d4c917304153a636883b24a933d06766b85b8
Sam Rogers <sam.rogers.kiwi@gmail.com>
2023-09-22 15:26:27 +1200

    working but ugly

--- javascript.js
+++ javascript.js
@@ -1,12 +1,55 @@
+// create grid generator
 let container = document.querySelector('.container');
-let square = document.createElement('div');
-square.style.width = '25px';
-square.style.height = '25px';
-for(let i = 1; i < 257; i++){
-    let square = document.createElement('div');
-    square.style.width = '14px';
-    square.style.height = '14px';
-    
-    square.setAttribute("id", "cell");
-    container.appendChild(square);
+let square;
+let resolution = 32;
+let selection = 'mouseover';
+let color = 'rainbow';
+let cellId = [];
+//container.setAttribute("style", `width: ${(resolution * resolution)}px; height: ${(resolution * resolution)}px;`);
+function gridGen() {
+    for(let i = 1; i < ((resolution * resolution) + 1); i++){
+        square = document.createElement('div');
+        square.style.width = `${256 / resolution}px`;
+        square.style.height = `${256 / resolution}px`;
+        square.setAttribute("class", "cell");
+        square.setAttribute("id", `${i}`);
+        cellId[i] = square;
+        container.appendChild(square);
+        square.addEventListener(selection, () => {
+            let clickedSquare = document.getElementById(i);
+            if (color == 'rainbow'){
+                clickedSquare.style.backgroundColor = rgba();
+            }
+            else {
+                clickedSquare.style.backgroundColor = color;
+            }
+        })
+    }
 }
+let hoverButton = document.querySelector('.hover');
+let clickButton = document.querySelector('.click');
+let resetButton = document.querySelector('.reset');
+hoverButton.addEventListener('click', () => {
+    selection = 'mouseover';
+    hoverButton.setAttribute("style", "display: none;");
+    clickButton.setAttribute("style", "display: none;");
+    gridGen();
+})
+clickButton.addEventListener('click', () => {
+    selection = 'mousedown';
+    hoverButton.setAttribute("style", "display: none;");
+    clickButton.setAttribute("style", "display: none;");
+    gridGen();
+})
+resetButton.addEventListener('click', () => {
+    location.reload();
+})
+function rgba() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    return(`rgb(${red},${green},${blue})`);
+}
+
+
+
